97a82c160441599ac1fbab62b77dd28ae5172762
Federico Morales <federico.morales@neoris.cxnetworks.net>
2018-09-11 12:25:43 -0300

    Tp1 Calculadora
    Se crean las clases Entidades y Calculadora y se comienza con su
    desarrollo.

---
+++ TP 1/Entidades/Entidades/Calculadora.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        /// <summary>
+        /// Valida y realiza la operación pedida entre ambos Numeros.
+        /// </summary>
+        /// <param name="nro1">Primer Numero.</param>
+        /// <param name="nro2">Segundo Numero.</param>
+        /// <param name="operador">Parámetro que indica la operación a realizar. En caso de invalidez se asigna "+".</param>
+        /// <returns>Retorna el valor del resultado de la operación.</returns>
+        public double Operar (Numero nro1, Numero nro2, string operador)
+        {
+            double resultado;
+            string operadorValidado = ValidarOperador(operador);
+
+            switch (operadorValidado)
+            {
+                case "+":
+                    resultado = nro1 + nro2;
+                    break;
+                case "-":
+                    resultado = nro1 - nro2;
+                    break;
+                case "*":
+                    resultado = nro1 * nro2;
+                    break;
+                case "/":
+                    resultado = nro1 / nro2;
+                    break;
+                default:
+                    resultado = 0;
+                    break;
+            }
+            return resultado;
+        }
+
+        /// <summary>
+        /// Valida que el parámetro operador sea "+", "-", "/" o "*".
+        /// </summary>
+        /// <param name="operador"></param>
+        /// <returns>Si el parámetro es válido lo retorna, Caso contrario retornará "+".</returns>
+        private static string ValidarOperador(string operador)
+        {
+            // Ver si el switch con un string null o empty toma el valor default.
+            string operadorValidado = "+";
+            if (!string.IsNullOrEmpty(operador))
+            {
+                switch (operador)
+                {
+                    case "+":
+                        operadorValidado = "+";
+                        break;
+                    case "-":
+                        operadorValidado = "-";
+                        break;
+                    case "*":
+                        operadorValidado = "*";
+                        break;
+                    case "/":
+                        operadorValidado = "/";
+                        break;
+                    default:
+                        operadorValidado = "+";
+                        break;
+                }
+            }
+            return operadorValidado;
+        }
+
+
+    }
+}
---
+++ TP 1/Entidades/Entidades/Numero.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero;
+
+        public Numero() : this(0) { }        
+
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero)
+        {
+            SetNumero(strNumero);
+        }
+
+        /// <summary>
+        /// Asigna un valor al atributo numero, previa validación.
+        /// </summary>
+        /// <param name="strNumero"></param>
+        public void SetNumero(string strNumero)
+        {
+            this.numero = ValidarNumero(strNumero);
+        }
+
+        /// <summary>
+        ///  Comprueba que el valor recibido sea numérico.
+        /// </summary>
+        /// <param name="strNumero"></param>
+        /// <returns>Valor en formato double. Si el parámetro pasado es inválido retorna cero.</returns>
+        private double ValidarNumero (string strNumero)
+        {
+            double valor = 0;
+            if (!string.IsNullOrEmpty(strNumero))
+            {
+                double.TryParse(strNumero, out valor);
+            } 
+            return valor;
+        }
+
+        /// <summary>
+        /// Convierte un número binario a decimal.
+        /// </summary>
+        /// <param name="binario"></param>
+        /// <returns>Si es posible retorna el valor en decimal con formato string. En caso contrario retorna "Valor inválido". </returns>
+        public string BinarioDecimal(string binario)
+        {
+            return "";
+        }
+
+        /// <summary>
+        ///  Convierte un número de decimal a binario.
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns>Valor en formato string. Si no es posible la conversión retorna "Valor inválido".</returns>
+        public string DecimalBinario(string numero)
+        {
+            return "";
+        }
+
+        /// <summary>
+        ///  Convierte un número de decimal a binario.
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns>Valor en formato string. Si no es posible la conversión retorna "Valor inválido".</returns>
+        public string DecimalBinario(double numero)
+        {
+            return "";
+        }
+
+        // ver si corresponde 
+        //public static implicit operator double(Numero nro)
+        //{
+        //    return nro.numero;
+        //}
+
+        public static double operator -(Numero nro1, Numero nro2)
+        {
+            return nro1.numero - nro2.numero;
+        }
+
+        public static double operator +(Numero nro1, Numero nro2)
+        {
+            return nro1.numero + nro2.numero;
+        }
+
+        public static double operator *(Numero nro1, Numero nro2)
+        {
+            return nro1.numero * nro2.numero;
+        }
+
+        public static double operator /(Numero nro1, Numero nro2)
+        {
+            double resultado = 0;
+            if (nro2.numero != 0) resultado = nro1.numero / nro2.numero;
+            return resultado;
+        }
+    }
+}
