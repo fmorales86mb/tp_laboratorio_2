34fd0f4a3e7b77c376904b061215a282957d752d
Federico Morales <federico.morales@neoris.cxnetworks.net>
2018-11-04 20:58:16 -0300

    TP 3
    Se implementan las clases del proyecto Archivos.

--- TP 3/Morales.Federico.2D.TP3/Archivos/Texto.cs
+++ TP 3/Morales.Federico.2D.TP3/Archivos/Texto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 //Archivos:
 // Generar una interfaz con las firmas para guardar y leer.
@@ -10,7 +11,41 @@ using System.Threading.Tasks;
 
 namespace Archivos
 {
-    public class Texto
+    public class Texto : IArchivo<string>
     {
+        public bool Guardar(string archivo, string datos)
+        {
+            StreamWriter writer;
+
+            try
+            {
+                writer = File.CreateText(archivo);
+                writer.Write(datos);
+                writer.Close();
+                return true;
+            }
+            catch(Exception)
+            {                
+                return false;
+            }            
+        }
+
+        public bool Leer(string archivo, out string datos)
+        {
+            StreamReader reader;
+
+            try
+            {
+                reader = File.OpenText(archivo);
+                datos = reader.ReadToEnd();
+                reader.Close();
+                return true;
+            }
+            catch(Exception)
+            {
+                datos = string.Empty;
+                return false;
+            }
+        }
     }
 }
---
+++ TP 3/Morales.Federico.2D.TP3/Archivos/Xml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Archivos
+{
+    public class Xml<T> : IArchivo<T>
+    {
+        public bool Guardar(string archivo, T datos)
+        {
+            XmlTextWriter writer;
+            XmlSerializer serializer;
+
+            try
+            {
+                writer = new XmlTextWriter(archivo, Encoding.Default);
+                serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(writer, datos);
+                writer.Close();
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Leer(string archivo, out T datos)
+        {
+            XmlTextReader reader;
+            XmlSerializer serializer;
+
+            try
+            {
+                reader = new XmlTextReader(archivo);
+                serializer = new XmlSerializer(typeof(T));
+                datos = (T)serializer.Deserialize(reader);
+                reader.Close();
+                return true;
+            }
+            catch(Exception)
+            {
+                datos = default(T);
+                return false;
+            }
+        }
+    }
+}
